9bb48a1716c328f38e204787fc9ad0de105148c5
bhavanasikharam <bhavanasikharam55@gmail.com>
2025-03-10 23:12:14 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 import image1 from "./images/image1.png";
 import bugdha2 from "./images/bugdha2.png";
-import { FaHeart,FaShareAlt, FaEye } from "react-icons/fa"; 
-import { FaArrowRight } from "react-icons/fa"; 
+import { FaHeart, FaShareAlt, FaEye, FaArrowRight } from "react-icons/fa";
 
 const App = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [likes, setLikes] = useState(0);
-  const shares = 120; 
+  const [showMore, setShowMore] = useState(false); // State for Read More functionality
+  const shares = 120;
   const views = 27;
 
   return (
@@ -20,51 +20,59 @@ const App = () => {
         </button>
         {isSidebarOpen && (
           <div className="content">
-            <h4>MAITREYA BUDDHA IN GESTURE OF FEARLESSNESS (ABHAYA MUDRA)</h4>
-     
-  {/* Icon Section */}
-        <div className="icon-section">
-  {/* Like Button */}
-  <div className="icon" style={{ display: "flex", flexDirection: "column", alignItems: "center" }} onClick={() => setLikes(likes + 1)}>
-  <FaHeart />
-  <span>{likes}</span>
-  </div>
+            <h4 style={{ color: "#333" }}>MAITREYA BUDDHA IN GESTURE OF FEARLESSNESS (ABHAYA MUDRA)</h4>
 
-   {/* Share Count */}
-         <div className="icon" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-    <FaShareAlt size={30} /> {/* Share Icon with custom color */}
-    <span>{shares}</span>
-  </div>
+            {/* Icon Section */}
+            <div className="icon-section">
+              {/* Like Button */}
+              <div className="icon" style={{ display: "flex", flexDirection: "column", alignItems: "center" }} onClick={() => setLikes(likes + 1)}>
+                <FaHeart />
+                <span>{likes}</span>
+              </div>
 
-  {/* View Count */}
-  <div className="icon" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-  <FaEye />
-  <span>{views}</span>
-  </div>
+              {/* Share Count */}
+              <div className="icon" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <FaShareAlt size={30} />
+                <span>{shares}</span>
+              </div>
 
-         </div>
+              {/* View Count */}
+              <div className="icon" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <FaEye />
+                <span>{views}</span>
+              </div>
+            </div>
+
             <p><strong>Dynasty:</strong> Ahichchatra</p>
             <p><strong>Period:</strong> 200 CE</p>
             <p><strong>Material:</strong> Sandstone</p>
             <p><strong>Location:</strong> National Museum</p>
-           
-           <h4>Description</h4>
-           <p>Maitreya, the feture Buddha to be, resides in the Tushita heaven as a bodhisattva waiting to redeem humanity.
-            in Buddhisattva, Maitreya is the eighth Buddha, a successor of the seven historical Buddhas (sapta-manushi Buddhas). 
-            The Digha Nikaya mention, Maitreya Buddha will be born in Ketumati, in present-day Varanasi, Uttar Pradesh. As a bodhisattva. 
-            Maitreya Wears a heavily adorened with earrings, Wristlets, necklaces, and an amulet.</p>
-       
-     <div style={{ display: "flex", alignItems: "center", color: "peru" }}>
-  <FaArrowRight style={{ marginRight: "8px", }} /> {/* Arrow mark before the text */}
-  <h5>READ MORE</h5>
-</div>
-      <button>ADD TO COLLECTION</button>
-      <button>SOUVENIR</button>
-  </div>
+
+            <h4 style={{ color: "#333" }}>Description</h4>
+            <p style={{ color: "#444" }}>
+              Maitreya, the future Buddha-to-be, resides in the Tushita heaven as a bodhisattva waiting to redeem humanity.
+              {showMore && (
+                <>
+                  {" "}In Buddhist traditions, Maitreya is the eighth Buddha, a successor of the seven historical Buddhas (sapta-manushi Buddhas).
+                  The Digha Nikaya mentions that Maitreya Buddha will be born in Ketumati, in present-day Varanasi, Uttar Pradesh.
+                  As a bodhisattva, Maitreya wears heavy adornments, including earrings, wristlets, necklaces, and an amulet.
+                </>
+              )}
+            </p>
+
+            {/* Read More / Read Less button */}
+            <div style={{ display: "flex", alignItems: "center", color: "peru", cursor: "pointer" }} onClick={() => setShowMore(!showMore)}>
+              <FaArrowRight style={{ marginRight: "8px" }} />
+              <h5>{showMore ? "READ LESS" : "READ MORE"}</h5>
+            </div>
+
+            <button>ADD TO COLLECTION</button>
+            <button>SOUVENIR</button>
+          </div>
         )}
-   </div>
-      
-  {/* Main Display */}
+      </div>
+
+      {/* Main Display */}
       <div className="main">
         <div className="image-container">
           <img src={image1} alt="Background" className="background" />
@@ -76,4 +84,3 @@ const App = () => {
 };
 
 export default App;
-
